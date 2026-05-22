cde6d791e14934c45b0bd33f2ab3a0467036e4e4
Miguel Angel Enriquez Rodrigueze <menriquezr@unal.edu.co>
2026-05-22 13:57:58 -0500

    Agrega proyecto STM32CubeIDE de tareas actualizadas a la semana 00

---
+++ TareasActualizadas_menriquezr/Src/main.c
@@ -0,0 +1,18 @@
+/**
+TAREAS ACTUALIZABLES DESDE SEMANA 0, MIGUEL ENRIQUEZ RODRIGUEZ
+ */
+
+#include <stdint.h>
+#include <stm32f411xe.h>
+
+
+#if !defined(__SOFT_FP__) && defined(__ARM_FP)
+  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
+#endif
+
+int main(void)
+{
+    /*SEMANA 0 */
+	/* Loop forever */
+	for(;;);
+}
